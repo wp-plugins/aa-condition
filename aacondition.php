8fce6ecb757138bd1e6701291aaadef07796f9ab
aaextention <aaextention@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-01 19:20:46 +0000

    Update
    git-svn-id: https://plugins.svn.wordpress.org/aa-condition/trunk@1017994 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ aacondition.php
@@ -0,0 +1,55 @@
+<?php 
+/**
+ * @package AA_Condition
+ * @version 1.0
+ */
+/*
+Plugin Name: AA Condition
+Plugin URI: http://wordpress.org/plugins/aa-condition/
+Description: It is a Custom Condition Plugin made by AA Extension . Just use shortcode [aa_condition_button] and then you can replace your condition from plugin file.
+Author: A. Roy / A. Mahmud
+Version: 1.0
+Author URI: http://webdesigncr3ator.com
+*/
+	
+//tell wordpress to register the demolistposts shortcode
+add_shortcode("aa_condition_button", "aaconditionbutton_handler");
+
+function aaconditionbutton_handler() {
+
+if ( is_user_logged_in() ) {
+   echo '<p style="text-align:center"><a href="#"><span style="font-size:14px"><input type="button" value="GET HELP" name="START A PROJECT" class="button-medium"></span></a></p>';
+} 
+
+
+if ( is_user_logged_in() || !is_user_logged_in() )
+{
+ echo '<p style="text-align:center"><a href="#"><span style="font-size:14px"><input type="button" value="GIVE BACK" name="START A PROJECT" class="button-medium"></span></a></p>';
+
+}
+
+if ( !is_user_logged_in() ) {
+echo '<p style="text-align:center; font-weight:bold;">If you would like to create a campaign please <span style="color:red">Log in</span> or <span style="color:red">Register</span> below.</p>';
+
+?>
+
+<style type="text/css">
+.login_block p{ display:inline-block; padding-right:15px;}
+.login_block { text-align:center; }
+
+</style>
+
+<div class="login_block">
+<p style="font-weight:bold;"><?php _e("Login", 'aa_extension'); ?></a></p>
+
+<p style=" font-weight:bold;"><?php _e("Register", 'aa_extension'); ?></a></p>
+</div>
+<?php 
+
+} 
+
+}
+
+
+
+
---
+++ readme.txt
@@ -0,0 +1,59 @@
+=== AA Condition Plugin ===
+	Contributors: aaextension
+	Donate link: http://webdesigncr3ator.com
+	Tags: aaextension, conditon, login, shortcode, aa-condition
+	Requires at least: 3.8
+	Tested up to: 4.0
+	Stable tag: 1.0
+	License: GPLv2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+	It is a simple condition based plugin, which is made with user logged and not logged in condition.
+
+== Description ==
+
+
+This plugin is specially made for people who wants to put some condition according to user logged or guest position , I mean not logged in position. I hope you can easily change aacondition.php file condition and can use the plugin with shortcode and here is the shortcode for you [aa_condition_button]. Very simple to use. If you need to know anything then contact to us via email (contact2us.aa@gmail.com) . We are happily help you.
+
+
+### AA Condition Plugin by http://webdesigncr3ator.com
+
+<br />
+
+
+<strong>Plugin Features</strong><br />
+
+* Coming soon
+
+
+
+
+== Installation ==
+
+1. Install as regular WordPress plugin.<br />
+2. Go your plugin setting via WordPress Dashboard and find "<strong>AA Condition Plugin</strong>" activate it.<br />
+
+
+<br />
+
+
+and shortcode for you </strong> `[aa_condition_button]`<br />
+
+then paste this shortcode anywhere in your page to display your custom condition<br />
+
+
+
+
+
+
+
+== Screenshots ==
+
+1. screenshot-1
+
+
+== Changelog ==
+
+	
+	= 1.0 =
+    * 2/11/2014 Initial release.
